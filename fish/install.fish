a97534d695feacb155bb740f27583a4cb178e980
Wouter Horré <wouter.horre@virdata.com>
2014-09-24 09:53:40 +0200

    Symlink individual fish files
    Allows for adding local functions and variables

--- fish/install.fish
+++ fish/install.fish
@@ -1,5 +1,13 @@
 #!/usr/bin/fish
 
+mkdir -p ~/.config/fish/functions
+mkdir -p ~/.config/fish/variables
+
 ln -s $PWD/config.fish ~/.config/fish/config.fish
-ln -sT $PWD/functions ~/.config/fish/functions
-ln -sT $PWD/variables ~/.config/fish/variables
+for f in (ls functions)
+	ln -s $PWD/functions/$f ~/.config/fish/functions/$f
+end
+
+for f in (ls variables)
+	ln -s $PWD/variables/$f ~/.config/fish/variables/$f
+end
