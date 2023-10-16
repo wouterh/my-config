5007a02ab7894477605d59e4465b41773aefe41a
Wouter Horré <wouter@versasense.com>
2023-10-16 13:42:07 +0200

    Fix script to activate home desk

--- bin/marvin/activate-home-desk.sh
+++ bin/marvin/activate-home-desk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-swaymsg output HDMI-A-1 mode 2560x1440 scale 1
-swaymsg output HDMI-A-1 position -2560 0
+swaymsg 'output HDMI-A-1 mode 2560x1440 scale 1'
+swaymsg 'output HDMI-A-1 position -2560 0'
 swaymsg '[workspace="3"] move workspace to output HDMI-A-1'
 swaymsg '[workspace="4"] move workspace to output HDMI-A-1'
 swaymsg '[workspace="5"] move workspace to output HDMI-A-1'
