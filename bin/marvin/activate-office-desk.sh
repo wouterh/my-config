3ff40d368b4d7df9b8c92d69ba1cb675a2da5129
Wouter Horré <wouter@versasense.com>
2023-11-21 10:44:16 +0100

    Add script to switch back to office setup

---
+++ bin/marvin/activate-office-desk.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+swaymsg 'output DP-3 position 1920 0'
+swaymsg 'output eDP-1 position 0 0'
+swaymsg '[workspace="3"] move workspace to output DP-3'
+swaymsg '[workspace="4"] move workspace to output DP-3'
+swaymsg '[workspace="5"] move workspace to output DP-3'
+swaymsg '[workspace="6"] move workspace to output DP-3'
+swaymsg '[workspace="7"] move workspace to output DP-3'
+swaymsg '[workspace="8"] move workspace to output DP-3'
+swaymsg '[workspace="10"] move workspace to output DP-3'
