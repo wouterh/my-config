cd27500805c17d02465b3ed4ce900524c5129562
Wouter Horré <wouter@versasense.com>
2025-01-20 09:47:21 +0100

    Add initialization for pyenv
    ... if pyenv executable is found

---
+++ .zsh.d/zsh/pyenv.zsh
@@ -0,0 +1,11 @@
+# Load pyenv automatically by appending
+# the following to
+# ~/.zprofile (for login shells)
+# and ~/.zshrc (for interactive shells) :
+
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+if command -v pyenv 2>&1 > /dev/null; then
+	eval "$(pyenv init -)"
+	eval "$(pyenv virtualenv-init -)"
+fi
